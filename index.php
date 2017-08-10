5358dafcc96141a2aeeaa8dbd4cb9a4c1e58872f
AbirDhar <rinky.raj.292@gmail.com>
2017-08-10 18:23:15 +0600

    class-01
    php-basic

---
+++ index.php
@@ -0,0 +1,31 @@
+ <?php
+
+
+/*
+Class 05 (2nd part):::::::::::: 
+*/
+
+
+/*logical comparison operators*/
+
+var_dump(10 > 4);      /*True*/
+var_dump(19 < 5);      /*False*/
+var_dump(10 >= 4);     /*True*/
+var_dump(19 <= 5);     /*False*/
+var_dump(10 == 10);    /*True*/
+var_dump("10" == 10);  /*True*/
+var_dump(8 != 5); 	   /*True*/
+var_dump(10 === 5);	   /*False*/
+var_dump("10" === 5);  /*False*/
+var_dump(10 !== 5);	   /*True*/
+
+
+
+/*
+Class 05 (1st part):::::::::::: 
+*/
+
+
+$name = 'Abir';   /*Assignment operator */
+$name .= 'Kumar';  /*Concatenation assaniment operator */
+echo $name
